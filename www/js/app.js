94da7b337626153675da6a84462e6adacd6199f8
Bajnok Tamas <tamas.bajnok@yahoo.co.uk>
2017-01-19 00:18:46 +0200

    Small design changes

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ion-autocomplete', 'ngOpenFB' ])
+angular.module('starter', ['ionic', 'starter.controllers', 'ion-autocomplete', 'ngOpenFB', 'ngCordova'])
 
 .run(function($ionicPlatform,ngFB) {
   ngFB.init({appId: '599219800249231'});
