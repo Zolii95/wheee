5569d929c0bdc7860edc6159976471890bb7e6cc
Geczi Zoltan <zolicsaba@yahoo.com>
2017-01-18 12:24:34 +0100

    Search for events is done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,17 +10,21 @@ var searched_location;
 angular.module('starter.controllers', ['ngOpenFB', 'ngMaterial'])
 
   .factory('EventDetail', function () {
- 
-    var eventdetail = {};
- 
-    return {
-        getEvObject: function () {
-            return eventdetail;
-        },
-        setEvObject: function (evObject) {
-            eventdetail = evObject;
-        }
-    };
+
+    var eventdetail = {};
+    eventdetail.cityId = "";
+    eventdetail.placeName = "";
+    eventdetail.placeId = "";
+
+    return eventdetail;
+    /*return {
+          getEvObject: function () {
+                return eventdetail;
+          },
+          setEvObject: function (evObject) {
+                eventdetail = evObject;
+          }
+    };*/
   })
 
   .controller('AppCtrl', function ($scope, $ionicModal, $timeout, ngFB, $http) {
@@ -358,7 +362,7 @@ angular.module('starter.controllers', ['ngOpenFB', 'ngMaterial'])
     self.cityChange = cityChange;
     self.eventChange = eventChange;
 
-    
+
 
     function searchTextChangeCity(text) {
 
@@ -388,6 +392,9 @@ angular.module('starter.controllers', ['ngOpenFB', 'ngMaterial'])
       $log.info('Item changed to ' + JSON.stringify(item));
       Event_Place = item.id;
       Event_PlaceName = item.value;
+      while (Event_PlaceName.indexOf(" ") != -1) {
+        Event_PlaceName = Event_PlaceName.replaceAt(Event_PlaceName.indexOf(" "), "+");
+      }
     }
 
     String.prototype.replaceAt = function (index, character) {
@@ -396,40 +403,7 @@ angular.module('starter.controllers', ['ngOpenFB', 'ngMaterial'])
 
     //When clicked on search button
     $scope.search = function () {
-      //$log.info('Search city: ' + City + ' Event pl: ' + Event_PlaceName + ' Country: ' + Country);
-      $scope.searchedLocation = [];
-      while (Event_PlaceName.indexOf(" ") != -1) {
-        Event_PlaceName = Event_PlaceName.replaceAt(Event_PlaceName.indexOf(" "), "+");
-      }
-
-      $scope.getSearch = function () {
-        $http.get("http://www.wheee.eu/api/event_search/events.php?last_searched_location=" + City + "&searched_local_name=" + Event_PlaceName + "&current_page=1")
-          .success(function (response) {
-            //$log.info(response.response);
-            if (response.response.length == 0) {
-              $http.get("http://www.wheee.eu/api/event_search/events.php?event_id=" + Event_Place)
-                .success(function (response2) {
-
-
-                  //searced_location = response2.response[Object.keys(response2.response)[0]];
-                  $log.info("Search controller");
-                  EventDetail.setEvObject(response2.response[Object.keys(response2.response)[0]]);
-                  
-
-                });
-            }
-            /*angular.forEach(response.response, function (event) {
-              $scope.topEvents.push(event);*/
-          });
-        location.href = '#/app/event_detail';
-      };
-
-      $scope.getSearch();
-      /*starter.controllers.factory('EventService', function () {
-                    return {
-                      name: searced_location.name
-                    };
-                  });*/
+      location.href = '#/app/event_detail';
     };
 
 
@@ -442,122 +416,145 @@ angular.module('starter.controllers', ['ngOpenFB', 'ngMaterial'])
       };
     }
   })
-  
 
 
-  .controller('DetailPage', function($scope, $log, $http, $sce, $ionicModal, EventDetail){
 
-    $scope.isLogged = localStorage.getItem("logged");
+  .controller('DetailPage', function ($scope, $log, $http, $sce, $ionicModal, $state, EventDetail) {
 
-    $scope.saveComment = function (commentForm) {
+    var EventDetails = {};
 
-      if(!$scope.isMessageAdded && commentForm.$$success.parse[0].$modelValue) {
-        var message = commentForm.$$success.parse[0].$modelValue;
-        $http.get('http://www.wheee.eu/api/event_detail/save_comment.php?event_id=38&user_id=' + $scope.isLogged + '&message=' + message)
-        .success(function (response) {
-          window.location.reload();
-        });
-        $scope.isMessageAdded = 1;
-      }
-    }
+    $http.get("http://www.wheee.eu/api/event_search/events.php?last_searched_location=" + City + "&searched_local_name=" + Event_PlaceName + "&current_page=1")
+      .success(function (response) {
+        //$log.info(response.response);
+        if (response.response.length == 0) {
+          $http.get("http://www.wheee.eu/api/event_search/events.php?event_id=" + Event_Place)
+            .success(function (response2) {
+              EventDetails = response2.response[Object.keys(response2.response)[0]];
+              $log.info(EventDetails);
+              
+              $scope.isLogged = localStorage.getItem("logged");
+              
+              $scope.saveComment = function (commentForm) {
 
-    $scope.getEvent = function () {
-      $scope.eventData = [];
-      $http.get('http://www.wheee.eu/api/event_search/events.php?event_id=38')
-        .success(function (response) {
-          $scope.eventData.push({
-            event_title: response.response[1].event_title,
-            event_start_date: response.response[1].event_start_date,
-            event_start_hour: response.response[1].event_start_hour,
-            local_name: response.response[1].local_name,
-            location: response.response[1].location,
-            county: response.response[1].county,
-            header_image: 'http://www.wheee.eu/upload/headers/' + response.response[1].header_image,
-            is_active: response.response[1].is_active
-          });
-          $scope.eventDescription = $sce.trustAsHtml(response.response[1].description);
-        });
+                if (!$scope.isMessageAdded && commentForm.$$success.parse[0].$modelValue) {
+                  var message = commentForm.$$success.parse[0].$modelValue;
+                  $http.get('http://www.wheee.eu/api/event_detail/save_comment.php?event_id=38&user_id=' + $scope.isLogged + '&message=' + message)
+                    .success(function (response) {
+                      window.location.reload();
+                    });
+                  $scope.isMessageAdded = 1;
+                }
+              }
+              
+              $scope.getEvent = function () {
+                $scope.eventData = [];
+                $http.get('http://www.wheee.eu/api/event_search/events.php?event_id=38')
+                  .success(function (response) {
+                    $scope.eventData.push({
+                      event_title: response.response[1].event_title,
+                      event_start_date: response.response[1].event_start_date,
+                      event_start_hour: response.response[1].event_start_hour,
+                      local_name: response.response[1].local_name,
+                      location: response.response[1].location,
+                      county: response.response[1].county,
+                      header_image: 'http://www.wheee.eu/upload/headers/' + response.response[1].header_image,
+                      is_active: response.response[1].is_active
+                    });
+                    $scope.eventDescription = $sce.trustAsHtml(response.response[1].description);
+                  });
+                  $log.info(EventDetails);
+                $scope.comments = [];
+                $http.get('http://www.wheee.eu/api/event_detail/get_comments.php?event_id=38&comment_limit=10')
+                  .success(function (response) {
+                    angular.forEach(response.response, function (comment) {
+                      $scope.comments.push(comment);
+                    });
+                  });
 
-      $scope.comments = [];
-      $http.get('http://www.wheee.eu/api/event_detail/get_comments.php?event_id=38&comment_limit=10')
-        .success(function (response) {
-          angular.forEach(response.response, function (comment) {
-            $scope.comments.push(comment);
-          });
-        });
+                $scope.images = [];
+                $http.get('http://www.wheee.eu/api/event_detail/get_images.php?event_id=38&image_limit=10')
+                  .success(function (response) {
+                    angular.forEach(response.response, function (image) {
+                      $scope.images.push(image);
+                    });
+                  });
+              };
+              $scope.getEvent();
 
-      $scope.images = [];
-      $http.get('http://www.wheee.eu/api/event_detail/get_images.php?event_id=38&image_limit=10')
-        .success(function (response) {
-          angular.forEach(response.response, function (image) {
-            $scope.images.push(image);
-          });
-        });
-    };
-    $scope.getEvent(); 
-    $log.info(EventDetail.getEvObject());
-    if($scope.isLogged) {
-      $scope.eventStatus = [];
-      $http.get('http://www.wheee.eu/api/event_search/get_event_status.php?event_id=38&user_id=' + $scope.isLogged)
-        .success(function (response) {
-          $scope.eventStatus.push({
-            is_joined: response.response.is_joined,
-            is_bookmarked: response.response.is_bookmarked
-          });
-        });
-    }
+              
 
-    $scope.changeJoinedStatus = function() {
-      $http.post('http://www.wheee.eu/api/event_search/save_event_status.php?joined=1&event_id=38&user_id=' + localStorage.getItem("logged"))
-        .success(function (response) {
-          window.location.reload();
-        });
-    }
-    
-    $scope.changeBookmarkedStatus = function() {
-      $http.post('http://www.wheee.eu/api/event_search/save_event_status.php?bookmark=1&event_id=38&user_id=' + localStorage.getItem("logged"))
-        .success(function (response) {
-          window.location.reload();
-        });
-    }
+              if ($scope.isLogged) {
+                $scope.eventStatus = [];
+                $http.get('http://www.wheee.eu/api/event_search/get_event_status.php?event_id=38&user_id=' + $scope.isLogged)
+                  .success(function (response) {
+                    $scope.eventStatus.push({
+                      is_joined: response.response.is_joined,
+                      is_bookmarked: response.response.is_bookmarked
+                    });
+                  });
+              }
 
-    // $scope.shareViaFacebook = function() {
-    //   var message = 'Test';
-    //   var logo = 'test';
-    //   var url = 'http://test.com'
-    //     $cordovaSocialSharing.canShareVia("facebook", message, logo, url).then(function(result) {
-    //         $cordovaSocialSharing.shareViaFacebook(message, logo, url);
-    //     }, function(error) {
-    //         alert(error)
-    //     });
-    // }
+              $scope.changeJoinedStatus = function () {
+                $http.post('http://www.wheee.eu/api/event_search/save_event_status.php?joined=1&event_id=38&user_id=' + localStorage.getItem("logged"))
+                  .success(function (response) {
+                    window.location.reload();
+                  });
+              }
 
-    $scope.openLoginModal = function() {
-      $ionicModal.fromTemplateUrl('login-modal.html', {
-        scope: $scope,
-        animation: 'splat'
-      }).then(function(modal) {
-        $scope.modal = modal;
-        $scope.modal.show();
+              $scope.changeBookmarkedStatus = function () {
+                $http.post('http://www.wheee.eu/api/event_search/save_event_status.php?bookmark=1&event_id=38&user_id=' + localStorage.getItem("logged"))
+                  .success(function (response) {
+                    window.location.reload();
+                  });
+              }
+
+              // $scope.shareViaFacebook = function() {
+              //   var message = 'Test';
+              //   var logo = 'test';
+              //   var url = 'http://test.com'
+              //     $cordovaSocialSharing.canShareVia("facebook", message, logo, url).then(function(result) {
+              //         $cordovaSocialSharing.shareViaFacebook(message, logo, url);
+              //     }, function(error) {
+              //         alert(error)
+              //     });
+              // }
+
+              $scope.openLoginModal = function () {
+                $ionicModal.fromTemplateUrl('login-modal.html', {
+                  scope: $scope,
+                  animation: 'splat'
+                }).then(function (modal) {
+                  $scope.modal = modal;
+                  $scope.modal.show();
+                });
+              }
+
+              $scope.closeLoginModal = function () {
+                $scope.modal.hide();
+              };
+              // Cleanup the modal when we're done with it!
+              // $scope.$on('$destroy', function() {
+              //   $scope.modal.remove();
+              // });
+              // Execute action on hide modal
+              $scope.$on('modal.hidden', function () {
+                // Execute action
+              });
+              // Execute action on remove modal
+              $scope.$on('modal.removed', function () {
+                // Execute action
+              });
+
+
+            });
+        }
+
       });
-    }
 
-    $scope.closeLoginModal = function() {
-      $scope.modal.hide();
-    };
-    // Cleanup the modal when we're done with it!
-    // $scope.$on('$destroy', function() {
-    //   $scope.modal.remove();
-    // });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    });
-    
+    //------------***********--------------*********------------------------
+
+
+    //$state.reload();
   })
 
   .controller('NewEvents', function ($scope, $http) {
